700826039f025ee29a02b3939dee898c17dab7f4
starcube2017 <52973358@qq.com>
2017-08-05 07:00:13 +0800

    no message

--- sTest.js
+++ sTest.js
@@ -43,7 +43,7 @@ var server = http.createServer((req, res) => {
 server.on('clientError', (err, socket) => {
 	socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
 });
-server.listen(80,"localhost")
+server.listen(8000,"localhost")
 function jsonTxtTojson(txt)
 {
 	for(cName in txt)
