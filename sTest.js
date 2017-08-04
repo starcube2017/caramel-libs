1d95ccac30746e99cae3fd9a336c58a40125a134
starcube2017 <52973358@qq.com>
2017-08-05 06:43:35 +0800

    修改路徑方法

--- sTest.js
+++ sTest.js
@@ -43,7 +43,7 @@ var server = http.createServer((req, res) => {
 server.on('clientError', (err, socket) => {
 	socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
 });
-server.listen(80,"caraq.herokuapp.com")
+server.listen(80,"112.64.217.102")
 function jsonTxtTojson(txt)
 {
 	for(cName in txt)
